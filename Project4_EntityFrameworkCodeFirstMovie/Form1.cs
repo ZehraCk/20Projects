bf98e66e6508bb3092fdccf0affbb4bdd2b82aba
Zehra Çetinkaya <cetinkayazehra13@gmail.com>
2026-09-19 23:19:47 +0300

    Project 4

--- Project4_EntityFrameworkCodeFirstMovie/Form1.cs
+++ Project4_EntityFrameworkCodeFirstMovie/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿
+using Project4_EntityFrameworkCodeFirstMovie.DAL.Context;
+using Project4_EntityFrameworkCodeFirstMovie.DAL.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +18,45 @@ namespace Project4_EntityFrameworkCodeFirstMovie
         public Form1()
         {
             InitializeComponent();
+        }
+
+        MovieContext context = new MovieContext();
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = context.Categories.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            Category category = new Category();
+            category.CategoryName = txtName.Text;
+            context.Categories.Add(category);
+            context.SaveChanges();
+            MessageBox.Show("İşlem başarılı");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var value = context.Categories.Find(id);
+            value.CategoryName = txtName.Text;
+            context.SaveChanges();
+            MessageBox.Show("işlem başarılı");
+
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var values = context.Categories.Find(id);
+            context.Categories.Remove(values);
+            context.SaveChanges();
+            MessageBox.Show("işlem başarılı");
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var values = context.Categories.Where(x => x.CategoryName == txtName.Text).ToList();
+            dataGridView1.DataSource = values;
         }
     }
 }
