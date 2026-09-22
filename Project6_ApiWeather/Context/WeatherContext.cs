8e0e95d31f925344f0c339b0e56e08b53cbc6bbc
Zehra Çetinkaya <cetinkayazehra13@gmail.com>
2026-09-22 22:29:03 +0300

    Project6

---
+++ Project6_ApiWeather/Context/WeatherContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project6_ApiWeather.Entities;
+
+namespace Project6_ApiWeather.Context
+{
+    public class WeatherContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ZEHRA\\SQLEXPRESS;Initial Catalog=Db6Project20;Integrated Security=True;MultipleActiveResultSets=True;TrustServerCertificate=True");
+        }
+        public DbSet<City> Cities { get; set; }
+    }
+}
---
+++ Project6_ApiWeather/Entities/City.cs
@@ -0,0 +1,11 @@
+﻿namespace Project6_ApiWeather.Entities
+{
+    public class City
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+        public string CityCountry { get; set; }
+        public decimal Temp { get; set; }
+        public string Detail { get; set; }
+    }
+}
